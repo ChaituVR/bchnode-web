28330eea034b71adea48a9b4177b550d4d1a2a14
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-10 04:00:50 +0000

    Deploy bitcoin-cash-node/bchnode-web to github.com/bitcoin-cash-node/bchnode-web.git:gh-pages

---
+++ static/js/custom.js
@@ -0,0 +1,1 @@
+$(".navbar-toggler").click(function(){$(".navbar-toggler-icon").toggleClass("navbar-expanded")});var chevrons=["#about-selector","#lang-selector","#version-select"];chevrons.forEach(function(o,e){$(o).focus(function(){$(o+" > .chevron-down").removeClass("unrotate").addClass("rotate")}),$(o).blur(function(){$(o+" > .chevron-down").removeClass("rotate").addClass("unrotate")})});
